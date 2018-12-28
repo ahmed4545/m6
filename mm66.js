71d32c460dc76537b065650ee38b494d67e9137d
ahmed4545 <34707771+ahmed4545@users.noreply.github.com>
2018-12-28 21:24:20 +0200

    Create mm66.js

---
+++ mm66.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Welcome");
+
+var PrEfix = "m6";
+client.on('message', message => {
+  if (!message.content.startsWith(PrEfix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '389043015777517568') return;
+if (message.content.startsWith(PrEfix + 'wa')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(PrEfix + 'pl')) {
+client.user.setActivity(argresult, {type:'PLAYING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(PrEfix + 'ls')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(PrEfix + 'st')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+}
+
+});
+
+client.login(process.env.BOT_TOKEN);
