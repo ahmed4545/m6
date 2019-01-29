37d1b3bb8f71567ddce6decee1262df01afed737
ahmed4545 <34707771+ahmed4545@users.noreply.github.com>
2019-01-29 11:10:49 +0200

    Update mm66.js

--- mm66.js
+++ mm66.js
@@ -1,31 +1,15 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-console.log("Welcome");
-
-var PrEfix = "m6";
-client.on('message', message => {
-  if (!message.content.startsWith(PrEfix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== '389043015777517568') return;
-if (message.content.startsWith(PrEfix + 'wa')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-if (message.content.startsWith(PrEfix + 'pl')) {
-client.user.setActivity(argresult, {type:'PLAYING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-if (message.content.startsWith(PrEfix + 'ls')) {
-client.user.setActivity(argresult, {type:'LISTENING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-if (message.content.startsWith(PrEfix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-}
-
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag} !`);
+ 
 });
-
+ 
+client.on('guildMemberAdd',async member => {
+  if(member.guild.id !== '537968114340724746') return;
+  setTimeout(function(){
+  member.guild.channels.find(r => r.id === '537969708545343508').send('*Welcome to BubblesAvatar.*');
+},3000);
+});
 client.login(process.env.BOT_TOKEN);
